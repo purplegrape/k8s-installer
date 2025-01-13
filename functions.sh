2e5d9b6d9a27ef45881a089f3aa2b44edd1787b1
purplegrape <purplegrape4@gmail.com>
2025-01-13 16:41:15 +0800

    Update functions.sh

--- functions.sh
+++ functions.sh
@@ -13,3 +13,8 @@ setup_envoy_gateway(){
 setup_ingress_nginx(){
     kubectl apply -f k8s-yaml/ingress-nginx/v1.12.0/ingress-nginx-v1.12.0.yaml
 }
+
+setup_argocd(){
+    kubectl create namespace argocd
+    kubectl apply -n argocd -f k8s-yaml/argo-cd/v2.12/install.yaml
+}
